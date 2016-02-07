a8ec26306654f0101734315ae5462bc6992fa600
Nicolas <nico.jukic54@gmail.com>
2016-02-07 21:00:28 +0100

    integration of an API REST for retrieve all countries

---
+++ Oops-interfaces-library/src/main/java/com/gdf/singleton/PopulateDB.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gdf.singleton;
+
+import java.util.List;
+
+/**
+ *
+ * @author nicolas
+ */
+public interface PopulateDB {
+    
+    public List<String> getAllCountries();
+    
+}
